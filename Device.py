a77fea85c364b20474eb438ddb5b5e3a720ea6cd
unknown <michel.glazenborg@outlook.com>
2017-11-06 16:07:36 +0100

    'Working' connection

--- Device.py
+++ Device.py
@@ -2,59 +2,88 @@ import serial
 import time
 
 class Device():
-    def __init__ (self, name, port, sensorType):
-        self.name = name
-        self.port = port
-        self.sensorType = sensorType
-        self.status = 1 # 1 = rolled in, 0 = rolled out
-        if self.sensorType == "Light":
-            self.rolldown = 50
-        elif self.sensorType == "Temp":
-            self.rolldown == 22
-        self.establisch_connection()
+	def __init__ (self, name, port, sensorType, rollDown):
+		self.name = name
+		self.port = port
+		self.sensorType = sensorType
+		self.status = 1 # 1 = rolled in, 0 = rolled out
+		if rollDown != 0:
+			self.rolldown = rollDown
+		elif self.sensorType == "Light":
+			self.rolldown = 50
+		elif self.sensorType == "Temp":
+			self.rolldown == 22
+		self.establisch_connection()
 
-    def  set_rolldown(self, val):
-        self.rolldown = val
+	def set_rolldown(self, val):
+		self.rolldown = val
 
-    def establisch_connection(self):
-        self.ser = serial.Serial(self.port, 19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+	def establisch_connection(self):
+		self.ser = serial.Serial(self.port, 19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 
-    # Roll down shutter (Only available in manual mode)
-    def roll_down(self):
-        self.ser.write(b'\xff') # Add to code to trigger function on arduino
-        self.ser.write(b'\x30')
+	# Roll down shutter (Only available in manual mode)
+	def roll_down(self):
+		self.ser.write(b'\xff') # Add to code to trigger function on arduino
+		self.ser.write(b'\x30')
 
-    # Roll up shutter (Only available in manual mode)
-    def roll_up(self):
-        self.ser.write(b'\xff') # Add to code to trigger function on arduino
-        self.ser.write(b'\x20')
+	# Roll up shutter (Only available in manual mode)
+	def roll_up(self):
+		self.ser.write(b'\xff') # Add to code to trigger function on arduino
+		self.ser.write(b'\x20')
 
-    def get_status(self):
-        if self.status == 1:
-            return "Rolled in"
-        else:
-            return "Rolled out"
+	def get_status(self):
+		if self.status == 1:
+			return "Rolled in"
+		else:
+			return "Rolled out"
 
-    def receive(self):
-        transmission = int('{:08b}'.format(ord(self.ser.read())),2)
-        value = 0
-        if transmission == 0b01000000:
-            while True:
-                transmission = int('{:08b}'.format(ord(self.ser.read())),2)
-                if transmission == 0b01110000:
-                    break
-                else:
-                    value += transmission
-            value = value / 10
-            print(value) # Send value to graphs
-        elif transmission == 0b01010001:
-            self.status = 1
-        elif transmission == 0b01010000:
-            self.status = 0
+	def transmit(self, message):
+		self.ser.write(message)
+
+	def receive(self):
+		transmission = int('{:08b}'.format(ord(self.ser.read())),2)
+
+		if transmission != 0xff:
+			return
+
+		self.transmit(b'\b01100000')
+
+		value = 0
+		print('Original value:', transmission)
+		
+		while True:
+			transmission = int('{:08b}'.format(ord(self.ser.read())),2)
+			self.transmit(b'\b01100000')
+			print('Code: ', transmission)
+
+			if transmission == 0b01000000:
+				while True:
+					transmission = int('{:08b}'.format(ord(self.ser.read())),2)
+					self.transmit(b'\b01100000')
+					print('Value: ', transmission)
+
+					if transmission == 0b01110000:
+						break
+					else:
+						value += transmission
+				value /= 10
+				print('Calculated value: ', value)
+				break
+			elif transmission == 0b01010001:
+				self.status = 1
+				print('Status set to 1')
+				break
+			elif transmission == 0b01010000:
+				self.status = 0
+				print('Status set to 0')
+				break
+			
 
 #Test code
-#shutter_1 = Device("Attic", "COM5", "Light")
-#shutter_1.establisch_connection()
-#print("Connection established")
-#while True:
-#    shutter_1.receive()
+try:
+	shutter_1 = Device("Attic", "COM5", "Light", 0)		
+	print("Connection established")
+	while True:
+		shutter_1.receive()
+except:
+	print("Error: No device connected to port")
